f5b886a9dd19e35b8864c6f2a9050eac9039de4e
Sonivais <151126779+Sonivais@users.noreply.github.com>
2023-11-22 21:16:53 +0300

    Add files via upload
    Задание 3

---
+++ zadanie_3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(TextBox1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (!double.TryParse(TextBox2.Text, out double b))
+            { MessageBox.Show("Поле только для чисел"); }
+            double A;
+            double B;
+            double Z;
+            A = a;
+            B = b;
+            Z = A - B;
+            Answer.Text += "Стоимость 1кг шоколадных конфет " + A + "\n";
+            Answer.Text += "Стоимость 1кг ирисок " + B + "\n";
+            Answer.Text += "Разность " + Z;
+        }
+    }
+}
